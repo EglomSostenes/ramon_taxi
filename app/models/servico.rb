2a6126c6b4639cf5159e903fc780a80692c64cdd
EglomSostenes <eglomsostenes@gmail.com>
2020-01-28 00:33:26 -0300

    Serviços validados

--- app/models/servico.rb
+++ app/models/servico.rb
@@ -1,2 +1,5 @@
 class Servico < ApplicationRecord
+    validates :nome, presence: { message: "obrigatório" }
+    validates :email, presence: { message: "obrigatório" }
+    validates :fone, presence: { message: "obrigatório" }
 end
